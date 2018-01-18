4c7f8d5f0b24bf852113e937ac08bbafbee4401b
cch <709372193@qq.com>
2018-01-18 15:52:11 +0800

    依次导入所有依赖包

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import "normalize.css";
+import "./less/index.less";
 
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import ElementUi from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(ElementUi);
+
+import api from "./js/api-config.js";
+Vue.prototype.$api = api;
+
+import axios from "axios";
+axios.defaults.withCredentials = true;
+Vue.prototype.$http = axios;
 
 
 
